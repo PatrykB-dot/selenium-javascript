4ca6ebdecbf8b1b7cc4d6427d0b64067ca72e64b
PatrykB-dot <patrykfor3@gmail.com>
2020-10-08 14:39:17 +0200

    all

--- library.js
+++ library.js
@@ -7,6 +7,15 @@ async function main(){
 await driver.get('https://library-app.firebaseapp.com')
 await driver.findElement(By.css('input')).sendKeys('xxx@xxx.com');
 await driver.findElement(By.css('.btn-primary')).click();
-await driver.quit();
+await (await driver).findElements(By.css('li')).then(function(elements){
+    elements.map(function(element){
+        element.getText().then(function(txt){
+            console.log(txt);
+        })
+    })
+})
+setTimeout(()=>{
+    driver.quit();
+},3000) 
 }
 main();
